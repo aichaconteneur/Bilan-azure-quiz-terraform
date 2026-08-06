84159939a8a2c7410447cfbc880a68f5527ba4c7
aichaconteneur <aissatoukhairy45@gmail.com>
2026-08-06 15:50:00 +0200

    feat(terraform): add Azure Container Registry module

---
+++ starter/terraform/modules/container-registry/outputs.tf
@@ -0,0 +1,11 @@
+output "name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "id" {
+  value = azurerm_container_registry.acr.id
+}
+
+output "login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
